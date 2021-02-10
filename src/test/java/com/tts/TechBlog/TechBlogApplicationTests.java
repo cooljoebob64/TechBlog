95ef2bff60f04e9e3596698d170264219c9f4f28
Joshua Luppes <cooljoebob64@gmail.com>
2021-02-10 08:16:45 -0600

    Create base project files from Spring initializer

---
+++ src/test/java/com/tts/TechBlog/TechBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.TechBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
